42e23d236e1506496737e562b71edebaba7926e0
Evelyn <evelyn.chavez92@yahoo.com>
2021-08-16 11:44:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-let submitData = {
-    name: "Steve",
-    email: "steve@steve.com"
-};
-const configurationObject = {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-    },
-    body: JSON.stringify(submitData)
-}
-
-fetch ("http://localhost:3000/users", configurationObject)
-  .then(function(response) {
-      return response.json();
-       
-  })
- 
- 
+function submitData( name, email ) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+      .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }
