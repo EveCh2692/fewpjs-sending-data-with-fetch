70f44a11703701b87eb0a2fd407d0bd82e2a62be
Evelyn <evelyn.chavez92@yahoo.com>
2021-08-16 10:02:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+let submitData = {
+    name: "Steve",
+    email: "steve@steve.com"
+};
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(submitData)
+}
+
+fetch ("http://localhost:3000/users", configurationObject)
+  .then(function(response) {
+      return response.json();
+       
+  })
+ 
+ 
